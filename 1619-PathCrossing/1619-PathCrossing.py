5382954f695965871237f91693f2a00b6462c6f2
Raesah-Syed <65484128+Raesah-Syed@users.noreply.github.com>
2026-05-11 12:16:48 -0600

    Create 1619-PathCrossing

---
+++ 1619-PathCrossing/1619-PathCrossing.py
@@ -0,0 +1,30 @@
+# Last updated: 5/11/2026, 12:16:48 PM
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        O=[0,0]
+        x=0
+        y=set()
+        y.add(tuple(O))
+       
+        while(x<=len(path)-1):
+            if path[x]=='N':
+                O[0]=O[0]+1
+                #O[1]=O[1]+0
+            if path[x]=='S':
+                O[0]=O[0]-1
+                #O[1]=O[1]+0
+            if path[x]=='E':
+                #O[0]=O[0]+E[0]
+                O[1]=O[1]+1
+            if path[x]=='W':
+                #O[0]=O[0]+0
+                O[1]=O[1]-1
+            
+            if tuple(O) in y:
+                return True
+            else:
+                y.add(tuple(O))
+            x=x+1
+        return False
+        
+            
