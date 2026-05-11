6bee8e60fc27c0329888fabf3cd595ca73c6aa69
Raesah-Syed <65484128+Raesah-Syed@users.noreply.github.com>
2026-05-11 12:16:49 -0600

    Create 1523-CapitalGainloss

---
+++ 1523-CapitalGainloss/1523-CapitalGainloss.sql
@@ -0,0 +1,9 @@
+-- Last updated: 5/11/2026, 12:16:49 PM
+# Write your MySQL query statement below
+SELECT stock_name ,sum(p) as capital_gain_loss FROM 
+(
+SELECT stock_name,
+CASE WHEN operation ='Buy' THEN -price 
+ELSE price END as p
+FROM Stocks
+) as x group by stock_name
