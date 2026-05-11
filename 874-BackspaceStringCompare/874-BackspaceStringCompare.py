b2a1b7ffb5786a70e8da57f7a594cb4681e1d115
Raesah-Syed <65484128+Raesah-Syed@users.noreply.github.com>
2026-05-11 12:17:02 -0600

    Create 874-BackspaceStringCompare

---
+++ 874-BackspaceStringCompare/874-BackspaceStringCompare.py
@@ -0,0 +1,22 @@
+# Last updated: 5/11/2026, 12:17:01 PM
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        s1=[]
+        s2=[]
+        x=0
+        y=0
+        while x <= len(s)-1:
+            if s[x] != '#':
+                s1.append(s[x])
+            elif s1:  # <--- The Safety Check: Only pop if s1 is NOT empty
+                s1.pop()
+            x = x + 1
+        while y<=len(t)-1:
+            if t[y]!='#':
+                s2.append(t[y])
+            elif s2:
+                s2.pop()
+            y=y+1
+        if s1==s2:
+            return True
+        return False
