13b9aa08ca6f30d2cbb6d7fef639fe9b6d331dc2
Raesah-Syed <65484128+Raesah-Syed@users.noreply.github.com>
2026-05-13 23:41:10 -0600

    Create 1622-FancySequence

---
+++ 1622-FancySequence/version/python/1622-FancySequence_20260513_234109.py
@@ -0,0 +1,11 @@
+# Last updated: 5/13/2026, 11:41:09 PM
+# Iterate lengths
+1class Solution:
+2    def countNegatives(self, grid: List[List[int]]) -> int:
+3        c=0
+4        for i in range(len(grid)):
+5            for j in grid[i]:
+6                if j<0:
+7                    c+=1
+8        return c
+9        
