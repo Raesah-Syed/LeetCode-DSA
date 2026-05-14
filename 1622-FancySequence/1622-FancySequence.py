d06f7364f714ea897edf009039c29af227653f67
Raesah-Syed <65484128+Raesah-Syed@users.noreply.github.com>
2026-05-14 11:20:24 -0600

    File updated at 5/14/2026, 11:20:24 AM

--- 1622-FancySequence/1622-FancySequence.py
+++ 1622-FancySequence/1622-FancySequence.py
@@ -1,22 +1,14 @@
-# Last updated: 5/13/2026, 11:46:42 PM
+# Last updated: 5/14/2026, 11:20:24 AM
 1class Solution:
-2    def countNegatives(self, grid: List[List[int]]) -> int:
-3        rows = len(grid)
-4        cols = len(grid[0])
-5        
-6        # Start at bottom-left corner
-7        r = rows - 1
-8        c = 0
-9        count = 0
-10        
-11        while r >= 0 and c < cols:
-12            if grid[r][c] < 0:
-13                # If this is negative, everything to its right is negative!
-14                count += (cols - c)
-15                # Move up to the next row
-16                r -= 1
-17            else:
-18                # If this is positive, move right to find a negative
-19                c += 1
-20                
-21        return count
+2    def minimumBoxes(self, apple: List[int], capacity: List[int]) -> int:
+3        c=0
+4        t=0
+5        capacity.sort(reverse=True)
+6        print(capacity)
+7        for x in capacity:
+8            if c>=sum(apple):
+9                break
+10            else:
+11                c+=x
+12                t+=1
+13        return t
