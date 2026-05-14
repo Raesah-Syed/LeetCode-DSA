14a3a15383611e24e129eb011fb20a49d7d0a5c9
Raesah-Syed <65484128+Raesah-Syed@users.noreply.github.com>
2026-05-14 11:24:50 -0600

    File updated at 5/14/2026, 11:24:50 AM

--- 1622-FancySequence/1622-FancySequence.py
+++ 1622-FancySequence/1622-FancySequence.py
@@ -1,14 +1,15 @@
-# Last updated: 5/14/2026, 11:20:24 AM
+# Last updated: 5/14/2026, 11:24:50 AM
 1class Solution:
 2    def minimumBoxes(self, apple: List[int], capacity: List[int]) -> int:
 3        c=0
 4        t=0
-5        capacity.sort(reverse=True)
-6        print(capacity)
-7        for x in capacity:
-8            if c>=sum(apple):
-9                break
-10            else:
-11                c+=x
-12                t+=1
-13        return t
+5        sa=sum(apple)
+6        capacity.sort(reverse=True)
+7        print(capacity)
+8        for x in capacity:
+9            if c>=sa:
+10                break
+11            else:
+12                c+=x
+13                t+=1
+14        return t
