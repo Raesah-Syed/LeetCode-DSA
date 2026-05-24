07b74fcce7cc7074e4c0604280f428462ade6675
Raesah-Syed <65484128+Raesah-Syed@users.noreply.github.com>
2026-05-23 19:57:11 -0600

    Create 48-RotateImage

---
+++ 48-RotateImage/48-RotateImage.py
@@ -0,0 +1,15 @@
+# Last updated: 5/23/2026, 7:57:11 PM
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        for i in range(len(matrix)):
+            for j in range(i+1,len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+      
+        for k in range(len(matrix)):
+           matrix[k]=matrix[k][::-1]
+        return (matrix)
