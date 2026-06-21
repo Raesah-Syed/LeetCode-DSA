8891319ecf116dcd916fac25ff83bf0abfa155f9
Raesah-Syed <65484128+Raesah-Syed@users.noreply.github.com>
2026-06-20 23:50:50 -0600

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.py
@@ -0,0 +1,21 @@
+# Last updated: 6/20/2026, 11:50:49 PM
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        
+        if x<2:
+            return x
+
+        left,right=2,x//2
+
+        while left<=right:
+
+            mid=(left+right)//2
+
+            if mid*mid==x:
+                return mid
+            elif mid*mid<x:
+                left=mid+1
+            else:
+                right=mid-1
+        
+        return right
