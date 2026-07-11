e758fa98350160f65bf603af0c31839db9540414
Raesah-Syed <65484128+Raesah-Syed@users.noreply.github.com>
2026-07-11 15:22:38 -0600

    Create 108-ConvertSortedArraytoBinarySearchTree

---
+++ 108-ConvertSortedArraytoBinarySearchTree/version/python/108-ConvertSortedArraytoBinarySearchTree_20260711_152238.py
@@ -0,0 +1,21 @@
+# Last updated: 7/11/2026, 3:22:38 PM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+9        r=[]
+10        def tr(node):
+11            if not node:
+12                return
+13            
+14            tr(node.left)
+15            r.append(node.val)
+16            tr(node.right)
+17        
+18        tr(root)
+19        return r
+20
